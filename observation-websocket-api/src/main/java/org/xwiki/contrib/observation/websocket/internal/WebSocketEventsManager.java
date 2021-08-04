4a0072072f413f139b98edea14fbf47ed7fb3459
Thomas Mortagne <thomas.mortagne@gmail.com>
2021-08-04 11:42:46 +0200

    * Fix the build * Add beginning of the WebSocketEventsManager component

--- observation-websocket-api/src/main/java/org/xwiki/contrib/observation/websocket/internal/WebSocketEventsManager.java
+++ observation-websocket-api/src/main/java/org/xwiki/contrib/observation/websocket/internal/WebSocketEventsManager.java
@@ -19,11 +19,99 @@
  */
 package org.xwiki.contrib.observation.websocket.internal;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import javax.websocket.Session;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.observation.EventListener;
+import org.xwiki.observation.ObservationManager;
+import org.xwiki.observation.event.Event;
+
 /**
- * 
  * @version $Id$
  */
+@Component(roles = WebSocketEventsManager.class)
+@Singleton
 public class WebSocketEventsManager
 {
+    private static final String KEY_SESSION_LISTENER = "xwiki.observation.listeners";
 
+    @Inject
+    private ObservationManager observation;
+
+    private final AtomicLong idCounter = new AtomicLong();
+
+    class WebSocketEventListener implements EventListener
+    {
+        private final String name;
+
+        private final Session session;
+
+        private WebSocketEventListener(Session session)
+        {
+            this.name = "websocket-" + idCounter.incrementAndGet();
+            this.session = session;
+        }
+
+        @Override
+        public String getName()
+        {
+            return this.name;
+        }
+
+        @Override
+        public List<Event> getEvents()
+        {
+            return Collections.emptyList();
+        }
+
+        @Override
+        public void onEvent(Event event, Object source, Object data)
+        {
+            WebSocketEventsManager.this.onEvent(event, source, data, this.session);
+        }
+    }
+
+    public void addEvent(String json, Session session)
+    {
+        // TODO: Parse the message
+        Event event = null;
+
+        EventListener listener;
+        synchronized (session) {
+            // Register a listener for the session if it's not already the case
+            listener = (EventListener) session.getUserProperties().get(KEY_SESSION_LISTENER);
+            if (listener == null) {
+                listener = new WebSocketEventListener(session);
+                session.getUserProperties().put(KEY_SESSION_LISTENER, listener);
+            }
+        }
+
+        // Register the new event
+        this.observation.addEvent(listener.getName(), event);
+    }
+
+    public void dispose(Session session)
+    {
+        // Unregister the listener associated to the session
+        EventListener listener = (EventListener) session.getUserProperties().get(KEY_SESSION_LISTENER);
+
+        if (listener != null) {
+            this.observation.removeListener(listener.getName());
+        }
+    }
+
+    private void onEvent(Event event, Object source, Object data, Session session)
+    {
+        // TODO: Serialize the event and data as a message
+        String json = null;
+
+        // Send the message
+        session.getAsyncRemote().sendText(json);
+    }
 }
