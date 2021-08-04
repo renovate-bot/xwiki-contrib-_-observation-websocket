c340acf29afd9e9c328ca91a3d39a896d37551d1
Thomas Mortagne <thomas.mortagne@gmail.com>
2021-08-04 13:44:59 +0200

    New version of WebSocketEventsManager
    TODO:
    * unit tests
    * add support for events constructors with parameters

--- observation-websocket-api/src/main/java/org/xwiki/contrib/observation/websocket/internal/WebSocketEventsManager.java
+++ observation-websocket-api/src/main/java/org/xwiki/contrib/observation/websocket/internal/WebSocketEventsManager.java
@@ -19,18 +19,26 @@
  */
 package org.xwiki.contrib.observation.websocket.internal;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.websocket.Session;
 
+import org.slf4j.Logger;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.observation.EventListener;
 import org.xwiki.observation.ObservationManager;
 import org.xwiki.observation.event.Event;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * @version $Id$
@@ -39,10 +47,13 @@ import org.xwiki.observation.event.Event;
 @Singleton
 public class WebSocketEventsManager
 {
-    private static final String KEY_SESSION_LISTENER = "xwiki.observation.listeners";
+    private static final String KEY_SESSION_LISTENERS = "xwiki.observation.listeners";
 
     @Inject
     private ObservationManager observation;
+
+    @Inject
+    private Logger logger;
 
     private final AtomicLong idCounter = new AtomicLong();
 
@@ -50,11 +61,19 @@ public class WebSocketEventsManager
     {
         private final String name;
 
+        private final List<Event> events;
+
+        private final Object listenerData;
+
         private final Session session;
 
-        private WebSocketEventListener(Session session)
+        private WebSocketEventListener(Event event, Object listenerData, Session session)
         {
             this.name = "websocket-" + idCounter.incrementAndGet();
+            this.events = Collections.singletonList(event);
+
+            this.listenerData = listenerData;
+
             this.session = session;
         }
 
@@ -67,49 +86,73 @@ public class WebSocketEventsManager
         @Override
         public List<Event> getEvents()
         {
-            return Collections.emptyList();
+            return this.events;
         }
 
         @Override
         public void onEvent(Event event, Object source, Object data)
         {
-            WebSocketEventsManager.this.onEvent(event, source, data, this.session);
+            try {
+                WebSocketEventsManager.this.onEvent(event, source, data, this.listenerData, this.session);
+            } catch (JsonProcessingException e) {
+                logger.error("Failed to send the event asa websocket message", e);
+            }
         }
     }
 
-    public void addEvent(String json, Session session)
+    public void addEvent(Map<String, Object> message, Session session)
+        throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException,
+        InvocationTargetException, NoSuchMethodException, SecurityException
     {
-        // TODO: Parse the message
-        Event event = null;
+        // Parse the message
+        Map<String, Object> eventType = (Map) message.get("eventType");
+        Class<?> eventClass =
+            Class.forName((String) eventType.get("id"), true, Thread.currentThread().getContextClassLoader());
+        // TODO: add support for event parameters
+        Event event = (Event) eventClass.getConstructor().newInstance();
 
-        EventListener listener;
+        // Get the custom date
+        Object listenerData = message.get("eventData");
+
+        // Create the listener
+        WebSocketEventListener listener = new WebSocketEventListener(event, listenerData, session);
+
+        // Register the listener
+        this.observation.addListener(listener);
+
+        // Rembember the registered listener in the session
+        Map<String, EventListener> listeners;
         synchronized (session) {
-            // Register a listener for the session if it's not already the case
-            listener = (EventListener) session.getUserProperties().get(KEY_SESSION_LISTENER);
-            if (listener == null) {
-                listener = new WebSocketEventListener(session);
-                session.getUserProperties().put(KEY_SESSION_LISTENER, listener);
+            listeners = (Map<String, EventListener>) session.getUserProperties().get(KEY_SESSION_LISTENERS);
+            if (listeners == null) {
+                listeners = new ConcurrentHashMap<>();
+                session.getUserProperties().put(KEY_SESSION_LISTENERS, listeners);
             }
         }
-
-        // Register the new event
-        this.observation.addEvent(listener.getName(), event);
+        listeners.put(listener.getName(), listener);
     }
 
     public void dispose(Session session)
     {
         // Unregister the listener associated to the session
-        EventListener listener = (EventListener) session.getUserProperties().get(KEY_SESSION_LISTENER);
+        EventListener listener = (EventListener) session.getUserProperties().get(KEY_SESSION_LISTENERS);
 
         if (listener != null) {
             this.observation.removeListener(listener.getName());
         }
     }
 
-    private void onEvent(Event event, Object source, Object data, Session session)
+    private void onEvent(Event event, Object source, Object data, Object listenerData, Session session)
+        throws JsonProcessingException
     {
-        // TODO: Serialize the event and data as a message
-        String json = null;
+        // Serialize the event and data as a message
+        Map<String, Object> messageObject = new HashMap<>();
+        messageObject.put("event", event);
+        messageObject.put("source", source);
+        messageObject.put("data", data);
+        messageObject.put("eventData", listenerData);
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(messageObject);
 
         // Send the message
         session.getAsyncRemote().sendText(json);
